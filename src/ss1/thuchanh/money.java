7418806de31af918848d97eec2f6f19634cddc60
nvrqm <hung0964153699@gmail.com>
2024-11-27 20:54:20 +0700

    fixed

--- src/ss1/thuchanh/money.java
+++ src/ss1/thuchanh/money.java
@@ -6,11 +6,11 @@ public class money {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Amount of Dollars");
-        int dollar = sc.nextInt();
+        double dollar = sc.nextInt();
         if(dollar < 0){
             System.out.println("Invalid");
         } else {
-            int vnd = dollar*23000;
+            double vnd = dollar*23000;
             System.out.println("To VND is: " + vnd);
         }
 
