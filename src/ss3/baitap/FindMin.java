f987a1930cd2382e4790df863b138688ca0cfc6b
nvrqm <hung0964153699@gmail.com>
2024-12-02 20:43:10 +0700

    adjust

--- src/ss3/baitap/FindMin.java
+++ src/ss3/baitap/FindMin.java
@@ -12,6 +12,7 @@ public class FindMin {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
+
         int min = arr[0];
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] < min) {
